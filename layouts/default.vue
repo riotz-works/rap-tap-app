253001b974c4799e2dcd93b6b1b7de1d90719c05
lulzneko <lulzneko@riotz.works>
2018-10-16 14:41:21 +0900

    ✨ Introduce footer with a narrow row height

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@
       </v-container>
     </v-content>
 
-    <v-footer app>
+    <v-footer app class="footer" height="18px">
       <v-spacer></v-spacer>
       <small>© Since 2018 <a href="https://riotz.works">Riotz Works</a>.</small>
     </v-footer>
@@ -36,6 +36,15 @@ export default Vue.extend({
 
 <style>
 .title a {
+  color: #fff;
+  text-decoration: none;
+}
+
+.footer {
+  min-height: 0px;
+  text-align: right;
+}
+.footer a {
   color: #fff;
   text-decoration: none;
 }
