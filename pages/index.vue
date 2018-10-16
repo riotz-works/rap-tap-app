d8cd376411316a9da3c4d4cb06892f0b6a05f351
lopburny <lopburny@gmail.com>
2018-10-16 13:30:23 +0900

    ✨ Introduce outline index page with basic parts

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
   <section>
+
+      <!-- ロゴ表示 -->
+      <v-layout row >
+        <v-flex xs12>
+          <img src="@/static/icon.png" width="100%">
+        </v-flex>
+      </v-layout>
+
+      <!-- ボタン表示 -->
+      <v-layout row>
+        <v-flex xs12>
+          <v-btn dark color="black" class="host-btn">バトルを募集する</v-btn>
+          <v-btn dark color="black" class="host-btn">対戦する</v-btn>
+          <v-btn dark color="black" class="host-btn">観戦する</v-btn>
+        </v-flex>
+      </v-layout>
+
   </section>
 </template>
 
@@ -13,4 +30,9 @@ export default Vue.extend({
 
 
 <style scoped>
+.host-btn {
+  font-weight: 600;
+  margin: 0.5em 0;
+  width: 100%;
+}
 </style>
