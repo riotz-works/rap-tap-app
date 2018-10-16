005d4660b35c70638ff79547570f7aba6b5b3fe6
lopburny <lopburny@gmail.com>
2018-10-16 13:46:45 +0900

    ✨ Introduce rapper page UI parts

---
+++ pages/room/rapper.vue
@@ -0,0 +1,119 @@
+<template>
+  <section>
+
+      <!-- 参加人数、曲表示 -->
+      <v-layout row>
+        <v-flex xs12>
+          <v-chip label color="black" text-color="white" class="display-label">
+            <v-icon left>people</v-icon>0 人
+          </v-chip>
+          <br />
+          <v-chip label color="black" text-color="white" class="display-label">
+            <v-icon left>library_music</v-icon>Song
+          </v-chip>
+        </v-flex>
+      </v-layout>
+
+      <!-- 対戦パネル -->
+      <v-layout row>
+
+        <!-- 自分のパネル -->
+        <v-flex xs6 text-xs-center class="margin-0-2">
+          <v-card>
+            <video id="battle-movie-me" muted class="battle-movie"></video>
+            <v-card-title>
+              <div>nickname</div>
+            </v-card-title>
+            <v-card-actions>
+              <v-chip color="indigo" label outline>
+                <v-avatar>
+                  <v-icon>thumb_up</v-icon>
+                </v-avatar>
+                1
+              </v-chip>
+              <v-spacer></v-spacer>
+              <v-chip color="indigo" label outline>
+                <v-avatar>
+                  <v-icon>thumb_down</v-icon>
+                </v-avatar>
+                1
+              </v-chip>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
+        <!-- 相手のパネル -->
+        <v-flex xs6 text-xs-center class="margin-0-2">
+          <v-card>
+            <video id="battle-movie-competitor" muted class="battle-movie"></video>
+            <v-card-title>
+              <div>nickname</div>
+            </v-card-title>
+            <v-card-actions>
+              <v-chip color="indigo" label outline>
+                <v-avatar>
+                  <v-icon>thumb_up</v-icon>
+                </v-avatar>
+                1
+              </v-chip>
+              <v-spacer></v-spacer>
+              <v-chip color="indigo" label outline>
+                <v-avatar>
+                  <v-icon>thumb_down</v-icon>
+                </v-avatar>
+                1
+              </v-chip>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
+      </v-layout>
+
+      <!-- チャット -->
+      <v-layout row>
+        <v-flex xs12 class="margin-0-2">
+          <v-card>
+            <v-card-actions>
+              <v-text-field
+                :counter="20"
+                append-icon="chat"
+                label="メッセージを入力してください"
+                required
+              >
+              </v-text-field>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+
+  </section>
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+
+});
+</script>
+
+
+<style scoped>
+
+.battle-movie {
+  width: 100%;
+  height: auto; /* TODO: Set calculated max-height to keep aspect ratio */
+}
+
+.display-label {
+  margin: 0.5em 0;
+  width: 100%;
+}
+
+.margin-0-2 {
+  margin: 0.2em;
+}
+
+</style>
