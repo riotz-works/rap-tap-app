265966bf04707729df249585ff3d2fd734fc485b
CircleCI <info@riotz.works>
2018-10-16 07:12:27 +0000

    Update Site [ci skip]

--- sw.js
+++ sw.js
@@ -12,32 +12,32 @@ workbox.precaching.precacheAndRoute([
     "revision": "ae03145919c54b8d31062d5a6e0a348c"
   },
   {
-    "url": "/rap-tap-app/static/manifest.c662c10c487bd503befe.js",
-    "revision": "8de48b1e9abbc1aaeb7725c85ba4dbcb"
+    "url": "/rap-tap-app/static/manifest.abce6d99555c39156718.js",
+    "revision": "92a77effa14c1d72d9461fd65bfdb250"
   },
   {
     "url": "/rap-tap-app/static/pages/about.b6a699a42550eb8da488.js",
     "revision": "736e620878af5b613859d67ab407a21f"
   },
   {
-    "url": "/rap-tap-app/static/pages/enter.e64eabd63abc795bd544.js",
-    "revision": "cbff9bc4e9de7ae0b5f09de7c15d22f4"
+    "url": "/rap-tap-app/static/pages/enter.6531a8cf8e391e496507.js",
+    "revision": "5c685e7a386a46f5884d88496e0497a3"
   },
   {
     "url": "/rap-tap-app/static/pages/host.1659e06d05e4b88ba47e.js",
     "revision": "1865bef61494add3238e3d36a63ecf07"
   },
   {
-    "url": "/rap-tap-app/static/pages/index.d22ed64b78b1ff2f8d03.js",
-    "revision": "d7187849c285f7cfcb1f6a7d7bd80977"
+    "url": "/rap-tap-app/static/pages/index.e669db8998fcb205185e.js",
+    "revision": "281c0fecf00b1a13297931db1882da10"
   },
   {
-    "url": "/rap-tap-app/static/pages/room/rapper.47d059ce3e90c91bfc4c.js",
-    "revision": "e597d27c16ce6a3cc0f0ff74370de307"
+    "url": "/rap-tap-app/static/pages/room/rapper.611b636bf1f4154258e4.js",
+    "revision": "ebafae92a31032f960498fd1479356a5"
   },
   {
-    "url": "/rap-tap-app/static/pages/room/watcher.933ea86096710aa0dca9.js",
-    "revision": "8b84bcb322b0e930f6b4ed66a7753b36"
+    "url": "/rap-tap-app/static/pages/room/watcher.030fed3d14d1d7b01086.js",
+    "revision": "4fdec53226669c4a2c1a4a8b20fa713d"
   },
   {
     "url": "/rap-tap-app/static/vendor.029213348089b39ef627.js",
