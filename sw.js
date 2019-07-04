cf966cd687116bd35982e42bfc2f1706223ba4d9
CircleCI <contact@riotz.works>
2019-07-04 18:52:16 +0900

    Update Site [ci skip]

--- sw.js
+++ sw.js
@@ -4,16 +4,16 @@ importScripts('/rap-tap-app/static/workbox.dev.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/rap-tap-app/static/app.a46d200f71bd2f12af88.js",
-    "revision": "c8583367e6261327279f57fd06468b69"
+    "url": "/rap-tap-app/static/app.d32baf7654c8942f202a.js",
+    "revision": "f297dcf866173845551ab5f9d2b403d1"
   },
   {
     "url": "/rap-tap-app/static/layouts/default.8d908eac2f7baf585fd0.js",
     "revision": "ae03145919c54b8d31062d5a6e0a348c"
   },
   {
-    "url": "/rap-tap-app/static/manifest.f63137dc1c932fe22ef2.js",
-    "revision": "95a4dd4fbc6b0a0f3f118c36514ae2f6"
+    "url": "/rap-tap-app/static/manifest.cfcf48292ae8e38b6545.js",
+    "revision": "3e830d3d2af8de8391ecd8f88cd291d9"
   },
   {
     "url": "/rap-tap-app/static/pages/about.114c21626c875c1bcbdb.js",
@@ -24,8 +24,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "b7715b70bbdbfc0a733e4ad9438f1e82"
   },
   {
-    "url": "/rap-tap-app/static/pages/host.268c6f8bc7c02c01700b.js",
-    "revision": "6965f9aa8f9323a75e205311512736e7"
+    "url": "/rap-tap-app/static/pages/host.0e16b2193856590fec95.js",
+    "revision": "dae93b2a64d4d7bc7d5da3702723508a"
   },
   {
     "url": "/rap-tap-app/static/pages/index.6757f44fee3127101d4e.js",
