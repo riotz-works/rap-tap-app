1938a2405aa2512f0664c5cebe79abab38098c79
CircleCI <contact@riotz.works>
2020-06-06 17:29:34 +0900

    Update Site [ci skip]

--- sw.js
+++ sw.js
@@ -4,15 +4,15 @@ importScripts('/rap-tap-app/static/workbox.dev.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/rap-tap-app/static/app.0cd7998f4e02fbdecd5f.js",
-    "revision": "8143e45c8c7b3ec77a89ca066a5d4464"
+    "url": "/rap-tap-app/static/app.574a84412c3cb5f0101b.js",
+    "revision": "b97240135d285dd5138ba7ae9b35d42b"
   },
   {
     "url": "/rap-tap-app/static/layouts/default.aae57794fc30fc99f73f.js",
     "revision": "8a76b4d33502743fe725fbfc19881497"
   },
   {
-    "url": "/rap-tap-app/static/manifest.8d9a7b5c688306748fce.js",
+    "url": "/rap-tap-app/static/manifest.e7aa369fb0ebc0a2118f.js",
     "revision": "2140a0e585642823d69ff28cf84254ce"
   },
   {
